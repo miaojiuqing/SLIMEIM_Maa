cf86f40c5800087725b808d881e352b692169e84
miaojiuqing <ll13400709119@163.com>
2025-05-26 19:27:05 +0800

    文字识别库修改完繁中

--- configure.py
+++ configure.py
@@ -7,14 +7,16 @@ assets_dir = Path(__file__).parent / "assets"
 
 def configure_ocr_model():
     if not (assets_dir / "MaaCommonAssets" / "OCR").exists():
-        print("Please clone this repository completely, don’t miss \"--recursive\", and don’t download the zip package!")
-        print("请完整克隆本仓库，不要漏掉 \"--recursive\"，也不要下载 zip 包！")
+        print(
+            'Please clone this repository completely, don’t miss "--recursive", and don’t download the zip package!'
+        )
+        print('请完整克隆本仓库，不要漏掉 "--recursive"，也不要下载 zip 包！')
         exit(1)
 
     ocr_dir = assets_dir / "resource" / "model" / "ocr"
-    if not ocr_dir.exists():   # copy default OCR model only if dir does not exist
+    if not ocr_dir.exists():  # copy default OCR model only if dir does not exist
         shutil.copytree(
-            assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v4" / "zh_cn",
+            assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v3" / "zh_tw",
             ocr_dir,
             dirs_exist_ok=True,
         )
